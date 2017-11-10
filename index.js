f56942faebd614707cc49835393a77c82fafe29f
Cara Kane <cara.kane@gmail.com>
2017-11-10 00:33:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,7 @@ function createFareMultiplier(multiplier) {
 const fareDoubler = createFareMultiplier(2)
 
 const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(arrayOfDrivers, function) {
+  function()
+}
